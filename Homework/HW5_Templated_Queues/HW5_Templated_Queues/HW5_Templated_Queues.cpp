43f475d213a4f8d46d6bb93e57619f5bdb6ff8f0
Brandon Hawke (RIT Student) <bh1137@rit.edu>
2021-05-04 16:22:01 -0400

    started final

---
+++ Homework/HW5_Templated_Queues/HW5_Templated_Queues/HW5_Templated_Queues.cpp
@@ -0,0 +1,14 @@
+// HW5_Templated_Queues.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Queue.h";
+using namespace std; 
+
+
+int main()
+{
+    
+}
+
+
---
+++ Homework/HW5_Templated_Queues/HW5_Templated_Queues/Queue.h
@@ -0,0 +1,12 @@
+#pragma once
+template <class T>
+class Queue
+{
+public:
+	Queue();
+	void pushQueue();
+	void pop();
+	void print();
+	void getSize();
+	bool IsEmpty();
+};
